6db13afc88d10ca7888b3b43f7f3b95e836cb928
Adam Dawidziuk <adamdawidziuk@onet.pl>
2023-09-19 14:59:27 +0200

    Init project

---
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,52 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class QuizQuestionsActivity : AppCompatActivity()
+{
+    private var progressBar : ProgressBar? = null
+    private var tvProgress : TextView? = null
+    private var tvQuestion : TextView? = null
+    private var ivImage : ImageView? = null
+
+    private var tvOption1 : TextView? = null
+    private var tvOption2 : TextView? = null
+    private var tvOption3 : TextView? = null
+    private var tvOption4 : TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tvProgress)
+        tvQuestion = findViewById(R.id.tvQuestion)
+        ivImage = findViewById(R.id.ivImage)
+        tvOption1 = findViewById(R.id.tvOption1)
+        tvOption2 = findViewById(R.id.tvOption2)
+        tvOption3 = findViewById(R.id.tvOption3)
+        tvOption4 = findViewById(R.id.tvOption4)
+
+        val questionsList = Constants.getQuestions()
+        Log.e("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition" + "/" + progressBar?.getMax()
+
+        tvQuestion?.text = question!!.question
+        ivImage?.setImageResource(question.image)
+        tvOption1?.text = question.optionOne
+        tvOption2?.text = question.optionTwo
+        tvOption3?.text = question.optionThree
+        tvOption4?.text = question.optionFour
+
+    }
+}
